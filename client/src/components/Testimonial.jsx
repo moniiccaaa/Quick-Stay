e04be44a1c9b86d61d6c5db8584a2606a1c32587
Mahima Pandey <mahi.x0704@gmail.com>
2025-06-02 23:19:44 +0530

    Initial HomePage and Booking status

---
+++ client/src/components/Testimonial.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Title from './Title'
+import { testimonials } from '../assets/assets'
+import StarRating from './StarRating'
+
+const Testimonial = () => {
+  return (
+    <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 pt-20 pb-30'>
+        <Title 
+            title="What our guests say" 
+            subTitle="Discover why discerning travellers consistently
+            choose QuickStay for their exclusive and luxurious accommodation around the world."
+        />
+
+         <div className="flex flex-wrap items-center gap-1 mt-20">
+                {testimonials.map((testimonial) => (
+                    <div key={testimonial.id} className="bg-white p-6 rounded-xl shadow">
+                        <div className="flex items-center gap-3">
+                            <img className="w-10 h-10 rounded-full" src={testimonial.image} alt={testimonial.name} />
+                            <div>
+                                <p className="font-playfair text-xl">{testimonial.name}</p>
+                                <p className="text-gray-500">{testimonial.address}</p>
+                            </div>
+                        </div>
+                        <div className="flex items-center gap-1 mt-4">
+                            <StarRating/>
+                        </div>
+                        <p className="text-gray-500 max-w-[360px] mt-4">"{testimonial.review}"</p>
+                    </div>
+                ))}
+            </div>
+    </div>
+  )
+}
+
+export default Testimonial
